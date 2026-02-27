e30a13c0717f6bb2befaf6285a09acff57ea0ffb
danielhl96 <daniel.hennies@t-online.de>
2026-02-27 14:06:09 +0100

    feat: add error notification for account creation failure in ModalAccountAdd

--- src/Components/ModalAccountAdd.tsx
+++ src/Components/ModalAccountAdd.tsx
@@ -86,6 +86,12 @@ function ModalAccountAdd({
                 );
 
                 if (!isSuccessResponse(response)) {
+                  setStateNotifyManager({
+                    title: 'Error',
+                    message: 'Account creation failed',
+                    type: 'error',
+                  });
+
                   return;
                 }
                 setStateNotifyManager({
